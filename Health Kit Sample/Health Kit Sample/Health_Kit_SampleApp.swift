6e02f84312a2a6862f0c302804f1eadab5abd006
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-16 16:51:37 +0530

    Health Kit sample initial commit

---
+++ Health Kit Sample/Health Kit Sample/Health_Kit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Kit_SampleApp.swift
+//  Health Kit Sample
+//
+//  Created by Siddharth Kothari on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Health_Kit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
