50ebb41b82dde2e8049f77fc8b5edd25610d7523
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-16 14:57:41 +0530

    initial commit - QR image

---
+++ QR Image generator/QR Image generator/QR_Image_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Image_generatorApp.swift
+//  QR Image generator
+//
+//  Created by Siddharth Kothari on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct QR_Image_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
