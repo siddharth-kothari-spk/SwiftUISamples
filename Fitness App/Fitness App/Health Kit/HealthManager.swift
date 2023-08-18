d76f1d0fbf75ba771effc4ee0f57785947c2bcef
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-18 16:27:36 +0530

    added Health Manager to access HealthKit

---
+++ Fitness App/Fitness App/Health Kit/HealthManager.swift
@@ -0,0 +1,28 @@
+//
+//  HealthManager.swift
+//  Fitness App
+//
+//  Created by Siddharth Kothari on 18/08/23.
+//
+
+import Foundation
+import HealthKit
+
+class HealthManager: ObservableObject {
+ 
+    let healthStore = HKHealthStore()
+    
+    init() {
+        let steps = HKQuantityType(.stepCount)
+        
+        let healthTypes : Set = [steps]
+        
+        Task {
+            do {
+                try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
+            } catch {
+                print("error in fetching health data: \(error.localizedDescription)")
+            }
+        }
+    }
+}
