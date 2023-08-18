077acef3662e172db801cbe6e7d8a5891366e36f
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-18 15:59:07 +0530

    updated activity card and added 2 on HomeView for testing

--- Fitness App/Fitness App/ActivityCard.swift
+++ Fitness App/Fitness App/ActivityCard.swift
@@ -9,24 +9,28 @@ import SwiftUI
 
 struct ActivityCard: View {
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Daily Steps")
-                        .font(.system(size: 16))
-                    Text("Goal: 10000")
-                        .font(.system(size: 12))
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                Image(systemName: "figure.walk").foregroundColor(.green)
-            }
-            .padding()
+        ZStack {
+            Color(uiColor: .systemGray6)
+                .cornerRadius(15)
             
-            Text("5000").font(.system(size: 24))
-        }
-        .padding()
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Daily Steps")
+                            .font(.system(size: 16))
+                        Text("Goal: 10000")
+                            .font(.system(size: 12))
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    Image(systemName: "figure.walk").foregroundColor(.green)
+                }
+                .padding()
+                
+                Text("5000").font(.system(size: 24))
+            }
         .cornerRadius(15)
+        }
     }
 }
 
--- Fitness App/Fitness App/HomeView.swift
+++ Fitness App/Fitness App/HomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)) {
+                ActivityCard()
+                ActivityCard()
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
