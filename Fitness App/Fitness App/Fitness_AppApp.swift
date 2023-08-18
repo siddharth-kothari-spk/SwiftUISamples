9bcad338bc61ea7540a7886d606014516ae4a33a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-18 13:32:44 +0530

    Fitness App - initial commit

---
+++ Fitness App/Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Siddharth Kothari on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
