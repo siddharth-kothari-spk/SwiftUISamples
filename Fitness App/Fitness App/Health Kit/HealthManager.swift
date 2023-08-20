3ce54d288da34c1a1a2413ef64d940af4abadf13
siddharth-kothari-spk <siddharth.kothari.spk>
2023-08-20 13:03:08 +0530

    common workout method

--- Fitness App/Fitness App/Health Kit/HealthManager.swift
+++ Fitness App/Fitness App/Health Kit/HealthManager.swift
@@ -32,7 +32,7 @@ class HealthManager: ObservableObject {
                 fetchTodaySteps()
                 fetchTodayCalories()
                 fetchWorkoutWeekData()
-                fetchStrengthTrainingData()
+               // fetchStrengthTrainingData()
             } catch {
                 print("error in fetching health data: \(error.localizedDescription)")
             }
@@ -70,7 +70,7 @@ class HealthManager: ObservableObject {
             DispatchQueue.main.async {
                 self.activities["todayCalories"] = activity
             }
-            print("steps: \(calories)")
+            print("calories: \(calories)")
         }
         healthStore.execute(query)
     }
@@ -82,19 +82,131 @@ class HealthManager: ObservableObject {
         let workoutPredicate = HKQuery.predicateForWorkouts(with: .running)
         let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [timePredicate, workoutPredicate])
         let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 20, sortDescriptors: nil) { _, samples, error in
-            guard let workouts = samples as? [HKWorkout], error == nil else {
+            guard let workouts = samples as? [HKWorkout],workouts.count > 0, error == nil else {
                 print("error: \(String(describing: error?.localizedDescription))")
                 return
             }
-            var count = 0
+            var runningCount = 0
+            var strengthCount = 0
+            var stairsCount = 0
+            var stairClimbingCount = 0
+            var walkingCount = 0
+            
             for workout in workouts {
-                let duration = Int(workout.duration)/60
-                count += duration
                 print(workout.allStatistics, workout.duration, workout.workoutActivities, workout.workoutActivityType, workout.workoutEvents as Any)
+                
+                switch workout.workoutActivityType {
+                case .running:
+                    let duration = Int(workout.duration)/60
+                    runningCount += duration
+                case .traditionalStrengthTraining:
+                    let duration = Int(workout.duration)/60
+                    strengthCount += duration
+                case .stairs:
+                    let duration = Int(workout.duration)/60
+                    stairsCount += duration
+                case .stairClimbing:
+                    let duration = Int(workout.duration)/60
+                    stairClimbingCount += duration
+                case .walking:
+                    let duration = Int(workout.duration)/60
+                    walkingCount += duration
+                case .americanFootball,
+                        .archery,
+                        .australianFootball,
+                        .badminton,
+                        .baseball,
+                        .basketball,
+                        .bowling,
+                        .boxing,
+                        .climbing,
+                        .cricket,
+                        .crossTraining,
+                        .curling,
+                        .cycling,
+                        .dance,
+                        .danceInspiredTraining,
+                        .elliptical,
+                        .equestrianSports,
+                        .fencing,
+                        .fishing,
+                        .functionalStrengthTraining,
+                        .golf,
+                        .gymnastics,
+                        .handball,
+                        .hiking,
+                        .hockey,
+                        .hunting,
+                        .lacrosse,
+                        .martialArts,
+                        .mindAndBody,
+                        .mixedMetabolicCardioTraining,
+                        .paddleSports,
+                        .play,
+                        .preparationAndRecovery,
+                        .racquetball,
+                        .rugby,
+                        .rowing,
+                        .soccer,
+                        .squash,
+                        .sailing,
+                        .softball,
+                        .swimming,
+                        .snowboarding,
+                        .snowSports,
+                        .socialDance,
+                        .skatingSports,
+                        .surfingSports,
+                        .swimBikeRun,
+                        .tableTennis,
+                        .trackAndField,
+                        .volleyball,
+                        .waterPolo,
+                        .waterSports,
+                        .waterFitness,
+                        .wrestling,
+                        .wheelchairRunPace,
+                        .wheelchairWalkPace,
+                        .yoga,
+                        .barre,
+                        .coreTraining,
+                        .crossCountrySkiing,
+                        .downhillSkiing,
+                        .flexibility,
+                        .highIntensityIntervalTraining,
+                        .jumpRope,
+                        .kickboxing,
+                        .pilates,
+                        .taiChi,
+                        .mixedCardio,
+                        .handCycling,
+                        .discSports,
+                        .fitnessGaming,
+                        .cardioDance,
+                        .pickleball,
+                        .cooldown,
+                        .transition,
+                        .tennis,
+                        .stepTraining,
+                        .other:
+                    print("workout = \(workout.workoutActivityType.rawValue)")
+                @unknown default:
+                    print("unknown")
+                }
+                
             }
-            let activity = Activity(id: 2, title: "Running", subTitle: "Min this week", image: "figure.walk", amount: "\(count) minutes")
+            let runningActivity = Activity(id: 2, title: "Running", subTitle: "Min this week", image: "figure.walk", amount: "\(runningCount) minutes")
+            let strengthActivity = Activity(id: 3, title: "Strength Trainig", subTitle: "This week", image: "dumbbell", amount: "\(strengthCount) minutes")
+            let stairActivity = Activity(id: 4, title: "Stair Activity", subTitle: "This week", image: "stairs", amount: "\(stairsCount) minutes")
+            let stairClimbActivity = Activity(id: 5, title: "Stair Climb", subTitle: "This week", image: "figure.stairs", amount: "\(stairClimbingCount) minutes")
+            let walkingActivity = Activity(id: 6, title: "Walking", subTitle: "This week", image: "figure.walk.motion", amount: "\(walkingCount) minutes")
             DispatchQueue.main.async {
-                self.activities["weekRuning"] = activity
+                self.activities["weekRuning"] = runningActivity
+                self.activities["strengthTraining"] = strengthActivity
+                self.activities["stairActivity"] = stairActivity
+                self.activities["stairClimbActivity"] = stairClimbActivity
+                self.activities["walkingrActivity"] = walkingActivity
+                
             }
         }
         healthStore.execute(query)
