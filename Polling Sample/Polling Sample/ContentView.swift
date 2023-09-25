7a1d715d60cb2dde51345525e40eede2f2ddd2a2
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-25 09:15:21 +0530

    using polling in view

--- Polling Sample/Polling Sample/ContentView.swift
+++ Polling Sample/Polling Sample/ContentView.swift
@@ -9,15 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var pollingManager = PollingManager<String>(pollingInterval: 2.0)
+    @State var numberOfTimePolled = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Polling Example").padding()
+            Text("\(numberOfTimePolled)").padding()
+            Button("Stop polling") {
+                print("Polling stopped")
+                pollingManager.stopPolling()
+            }.padding()
+            Button("Start polling") {
+                pollingManager.startPolling {
+                    self.pollingAction()
+                }
+            }.padding()
+        }.onAppear {
+            pollingManager.startPolling {
+                self.pollingAction()
+            }
+        }.onDisappear {
+            pollingManager.stopPolling()
+        }.padding()
     }
+    
+    func pollingAction() {
+            // Perform your polling logic here
+            print("Polling...")
+            numberOfTimePolled += 1
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
