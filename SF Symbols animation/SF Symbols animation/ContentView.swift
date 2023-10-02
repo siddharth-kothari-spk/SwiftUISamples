eba095dfb3f38c640420649aeabd916e56df0e40
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-10-02 09:47:57 +0530

    added files for diff transitions

--- SF Symbols animation/SF Symbols animation/ContentView.swift
+++ SF Symbols animation/SF Symbols animation/ContentView.swift
@@ -6,18 +6,34 @@
 //
 
 // courtsey: https://blorenzop.medium.com/how-to-animate-sf-symbols-in-swiftui-c3b504af4f44
+// From iOS 17, Swift introduced a new modifier symbolEffect to perform pre-defined animations over SF Symbols.
+
+// We have seven built-in animations available to apply to any SF Symbol: Bounce, Pulse, Variable Color, Scale, Appear, Disappear, Replace
+/*
+ All animations are grouped into four types of behaviors
+
+     Discrete: the animation runs one time and ends.
+     Indefinite: the animation will continue until we remove it or disable it.
+     Transition: animate a symbol in or out of the view.
+     Content Transition: for animating a replacement of one symbol with another.
+ */
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            DiscreteView().tabItem {
+                Label("Discrete", systemImage: "square.and.arrow.up")
+            }
+            IndefiniteView().tabItem {
+                Label("Indefinite", systemImage: "square.and.arrow.up.circle")
+            }
+            TransitionView().tabItem {
+                Label("Transition", systemImage: "square.and.arrow.down")
+            }
+            ContentTransitionView().tabItem { Label("Content Transition", systemImage: "square.and.arrow.up.on.square") }
         }
-        .padding()
     }
 }
 
---
+++ SF Symbols animation/SF Symbols animation/Views/DiscreteView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscreteView.swift
+//  SF Symbols animation
+//
+//  Created by Siddharth Kothari on 02/10/23.
+//
+
+import SwiftUI
+
+struct DiscreteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiscreteView()
+}
