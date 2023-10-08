1db7e2371b7f50c18a2514aae570c4248fef2148
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-10-08 13:45:02 +0530

    using old and new location manager

--- Accessing User Location/Accessing User Location/ContentView.swift
+++ Accessing User Location/Accessing User Location/ContentView.swift
@@ -8,16 +8,34 @@
 // courtsey: https://holyswift.app/the-new-way-to-get-current-user-location-in-swiftu-tutorial/
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    
+    @State var oldLocationManager = OldLocationManager()
+    @State var newLocationManager = NewLocationManager()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding()
+            Text("Old Location Manager: \(oldLocationManager.location?.description ?? "No Location")")
+            Text("New Location Manager: \(newLocationManager.location?.description ?? "No Location")")
         }
         .padding()
+        .task {
+           try? await oldLocationManager.requestUserAuthorization()
+            oldLocationManager.startCurrentLocationUpdates()
+        }
+        .task {
+            try? await newLocationManager.requestUserAuthorization()
+            try? await newLocationManager.startCurrentLocationUpdates()
+        }
     }
 }
 
