a92ac14acf28fdb066abdf08573f645a28530dc7
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-10-08 13:29:19 +0530

    accessing location initial setup

---
+++ Accessing User Location/Accessing User Location/Accessing_User_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessing_User_LocationApp.swift
+//  Accessing User Location
+//
+//  Created by Siddharth Kothari on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Accessing_User_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessing User Location/Accessing User Location/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Accessing User Location
+//
+//  Created by Siddharth Kothari on 08/10/23.
+//
+
+// courtsey: https://holyswift.app/the-new-way-to-get-current-user-location-in-swiftu-tutorial/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
