f0c0a0a643a3f9ca85d45f9f01882a357144f32e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-10-09 22:19:20 +0530

    updated content view

--- LogicOnlyOnceOnViewAppear/LogicOnlyOnceOnViewAppear/ContentView.swift
+++ LogicOnlyOnceOnViewAppear/LogicOnlyOnceOnViewAppear/ContentView.swift
@@ -12,16 +12,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(content: {
+                Text("FirstView")
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .padding()
+                
+                NavigationLink("Next View") {
+                    View2()
+                }.buttonStyle(.borderedProminent)
+            })
         }
         .padding()
+        .onFirstAppear {
+            print("⭐️ On First Appear ! \(Date())")
+        }
+        .onAppear(perform: {
+            print("Run on Appear!!! \(Date())")
+        })
+        .onDisappear(perform: {
+            print("Run on Disappear!! \(Date())")
+        })
+        .task {
+            print("📀 Task print!")
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct View2: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("This is View 2")
+                    .padding()
+                Image(systemName: "star")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
+            .padding()
+        }
+    }
 }
