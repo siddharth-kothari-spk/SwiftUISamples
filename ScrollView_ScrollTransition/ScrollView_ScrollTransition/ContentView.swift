782161c12a12bba340b792f6361aed782c9926ed
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-10-20 09:47:39 +0530

    ScrollView_ScrollTransition initial commit

---
+++ ScrollView_ScrollTransition/ScrollView_ScrollTransition/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ScrollView_ScrollTransition
+//
+//  Created by Siddharth Kothari on 20/10/23.
+//
+// courtsey: https://santoshbotre01.medium.com/elevate-your-apps-user-experience-exploring-scrollview-scrolltransition-api-in-swiftui-with-ios-2c259025fab2
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrollView_ScrollTransition/ScrollView_ScrollTransition/ScrollView_ScrollTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_ScrollTransitionApp.swift
+//  ScrollView_ScrollTransition
+//
+//  Created by Siddharth Kothari on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_ScrollTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
