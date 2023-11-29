cc855cffcd9fad0f5d72c7cd6042477ebb484df1
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-11-29 18:45:16 +0530

    Glitch Effect - initial setup

---
+++ Glitch-Effect/Glitch-Effect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glitch-Effect
+//
+//  Created by Siddharth Kothari on 29/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glitch-Effect/Glitch-Effect/Glitch_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glitch_EffectApp.swift
+//  Glitch-Effect
+//
+//  Created by Siddharth Kothari on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Glitch_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
