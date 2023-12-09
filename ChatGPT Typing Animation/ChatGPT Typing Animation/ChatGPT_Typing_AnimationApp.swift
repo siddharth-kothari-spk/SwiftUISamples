0df9e4aa4cb1021804f150030b33576790d9cb6a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-09 09:13:13 +0530

    ChatGPT Typing Animation: input string added

---
+++ ChatGPT Typing Animation/ChatGPT Typing Animation/ChatGPT_Typing_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_Typing_AnimationApp.swift
+//  ChatGPT Typing Animation
+//
+//  Created by Siddharth Kothari on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_Typing_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
