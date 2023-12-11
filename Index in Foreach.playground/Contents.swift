685d776584ee23cf572f80a23ffdffe4227daec0
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-11 21:59:47 +0530

    Index in Foreach: 3. Using enumerated() method

--- Index in Foreach.playground/Contents.swift
+++ Index in Foreach.playground/Contents.swift
@@ -55,3 +55,26 @@ struct ContentView2: View {
      ForEach uses this range of indices. id: \.self is used to identify each element uniquely.
      Inside the ForEach loop, items[index] is used to access each item using the index.
  */
+
+
+// 3. Using enumerated() method
+struct ContentView3: View {
+    let items = ["Milk", "Bread", "Cookies", "Tea"]
+
+    var body: some View {
+        VStack {
+            ForEach(Array(items.enumerated()), id: \.1) { index, item in
+                Text("\(index): \(item)")
+            }
+        }
+    }
+}
+
+/*
+ In this example:
+
+     enumerated() is used to get a sequence of pairs (index, value) from the items array.
+     Array(items.enumerated()) converts the enumerated sequence into an array of tuples.
+     ForEach iterates through this array of tuples, where index and item represent the index and value, respectively.
+     id: \.1 is used to specify a unique identifier for each row in the ForEach loop. \0 refers to the first element of the tuple (which is the index) and \1 refers to the second element (which is the item).
+ */
