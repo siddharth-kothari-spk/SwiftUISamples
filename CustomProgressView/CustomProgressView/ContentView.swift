8668a206000dceda093d28dc4d95c92dbbcc2592
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-13 19:43:55 +0530

    CustomProgressView: initial commit

---
+++ CustomProgressView/CustomProgressView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomProgressView
+//
+//  Created by Siddharth Kothari on 13/12/23.
+//
+
+// courtsey: https://blog.stackademic.com/custom-progressview-in-swiftui-c94f51ba598b
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomProgressView/CustomProgressView/CustomProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomProgressViewApp.swift
+//  CustomProgressView
+//
+//  Created by Siddharth Kothari on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
