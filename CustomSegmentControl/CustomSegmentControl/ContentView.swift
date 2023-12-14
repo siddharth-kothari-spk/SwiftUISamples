303659580985dfe1441c786096a0e46382ffe620
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-14 19:24:48 +0530

    CustomSegmentControlApp: SegmentControl used in ContentView

--- CustomSegmentControl/CustomSegmentControl/ContentView.swift
+++ CustomSegmentControl/CustomSegmentControl/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedIndex1 = 0
+    @State var selectedIndex2 = 0
+    @State var selectedIndex3 = 0
+    
+    let options1: [SegmentControlItem] = [
+        SegmentControlItem(name: "Car"),
+        SegmentControlItem(name: "Bike")
+    ]
+    
+    let options2: [SegmentControlItem] = [
+        SegmentControlItem(name: "Car", iconString: "car.fill"),
+        SegmentControlItem(name: "Bike", iconString: "bicycle"),
+        SegmentControlItem(name: "Bus", iconString: "bus"),
+    ]
+    
+    let options3: [SegmentControlItem] = [
+        SegmentControlItem(iconString: "car.fill"),
+        SegmentControlItem(iconString: "bicycle"),
+        SegmentControlItem(iconString: "bus"),
+        SegmentControlItem(iconString: "airplane"),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            SegmentControl(
+                selectedIndex: $selectedIndex1,
+                options: options1
+            )
+            
+            SegmentControl(
+                selectedIndex: $selectedIndex2,
+                options: options2
+            )
+            
+            SegmentControl(
+                selectedIndex: $selectedIndex3,
+                options: options3
+            )
         }
         .padding()
     }
