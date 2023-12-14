e5d9abf1db5aa540c0b4dc38df5d0163b65595c5
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-14 19:18:01 +0530

    CustomSegmentControlApp: SegmentControl added

---
+++ CustomSegmentControl/CustomSegmentControl/SegmentControl.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentControl.swift
+//  CustomSegmentControl
+//
+//  Created by Siddharth Kothari on 14/12/23.
+//
+
+import SwiftUI
+
+struct SegmentControl: View {
+    @Binding var selectedIndex: Int
+    let options: [SegmentControlItem]
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
