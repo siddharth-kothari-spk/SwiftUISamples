5ba4abcd12439a3703f7a8dd8be4902fa5b23bc6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-25 21:45:11 +0530

    NavigationStack: initial commit

---
+++ NavigationStack/NavigationStack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by Siddharth Kothari on 25/12/23.
+//
+
+// courtsey: https://medium.com/@jpmtech/navigationstack-in-swiftui-d89a61bd8963
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Siddharth Kothari on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
