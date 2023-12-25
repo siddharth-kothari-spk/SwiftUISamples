51dbf4582b8fbef56934cdad7989f4b4e5a5aa50
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-25 21:46:52 +0530

    NavigationStack: DataModel and sample data

---
+++ NavigationStack/NavigationStack/DataModel.swift
@@ -0,0 +1,35 @@
+//
+//  DataModel.swift
+//  NavigationStack
+//
+//  Created by Siddharth Kothari on 25/12/23.
+//
+
+import Foundation
+
+import Foundation
+
+struct DataModel: Identifiable, Hashable {
+    let id = UUID()
+    let text: String
+}
+
+class SampleData {
+    static let firstScreenData = [
+        DataModel(text: "🚂 Trains"),
+        DataModel(text: "✈️ Planes"),
+        DataModel(text: "🚗 Automobiles"),
+    ]
+    
+    static let secondScreenData = [
+        DataModel(text: "Slow"),
+        DataModel(text: "Regular"),
+        DataModel(text: "Fast"),
+    ]
+    
+    static let lastScreenData = [
+        DataModel(text: "Wrong"),
+        DataModel(text: "So-so"),
+        DataModel(text: "Right"),
+    ]
+}
