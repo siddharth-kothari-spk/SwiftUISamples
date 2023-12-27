4dd0e6ad9acd57dd393875ee0fadbc37c0340b94
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-27 08:16:26 +0530

    NavigationSplitView: sample code for three column used

--- NavigationSplitView/NavigationSplitView/ContentView.swift
+++ NavigationSplitView/NavigationSplitView/ContentView.swift
@@ -17,13 +17,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ThreeColumnSampleCode()
     }
 }
 
