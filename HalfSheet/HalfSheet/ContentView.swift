21d2e08a20ae06dc52e17bf5a061f75f964c132c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-17 12:48:27 +0530

    HalfSheet: Initial commit

---
+++ HalfSheet/HalfSheet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HalfSheet
+//
+//  Created by Siddharth Kothari on 17/01/24.
+//
+
+// courtsey: https://21zerixpm.medium.com/swiftuis-half-sheet-for-sleek-user-interfaces-c3e77001d96f
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HalfSheet/HalfSheet/HalfSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfSheetApp.swift
+//  HalfSheet
+//
+//  Created by Siddharth Kothari on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HalfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
