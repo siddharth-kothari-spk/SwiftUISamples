edda46cff48e153a40f9a06f94f6f74cb784f3bb
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-18 16:58:04 +0530

    API Key Security: courtsey

---
+++ API Key Security/API Key Security/API_Key_SecurityApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Key_SecurityApp.swift
+//  API Key Security
+//
+//  Created by Siddharth Kothari on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct API_Key_SecurityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Key Security/API Key Security/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API Key Security
+//
+//  Created by Siddharth Kothari on 18/01/24.
+//
+
+// courtsey: https://medium.com/@kodekarim/api-key-security-in-ios-master-on-demand-protection-strategies-1aadd3c70413
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
