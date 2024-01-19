f9a91e848f0073c272a4dbd67d1f88e7f80247ff
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-19 16:47:46 +0530

    Test iOS Notification: courtsey added

---
+++ Test iOS Notification/Test iOS Notification/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test iOS Notification
+//
+//  Created by Siddharth Kothari on 19/01/24.
+//
+
+// courtsey: https://getstream.io/blog/test-ios-push-notifications/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test iOS Notification/Test iOS Notification/Test_iOS_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_iOS_NotificationApp.swift
+//  Test iOS Notification
+//
+//  Created by Siddharth Kothari on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_iOS_NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
