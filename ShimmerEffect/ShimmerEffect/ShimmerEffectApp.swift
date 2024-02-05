c1353a1df4b1106d77f98f451e1c04354354c7fd
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-05 08:02:07 +0530

    ShimmerEffect: initial commit

---
+++ ShimmerEffect/ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by Siddharth Kothari on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
