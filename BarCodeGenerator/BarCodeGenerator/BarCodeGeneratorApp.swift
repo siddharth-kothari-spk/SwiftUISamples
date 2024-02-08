5c13e9a9904732c2477fce40fa4d2b4e27af8598
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-08 12:12:34 +0530

    BarCodeGenerator: courtsey AppCoda

---
+++ BarCodeGenerator/BarCodeGenerator/BarCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeGeneratorApp.swift
+//  BarCodeGenerator
+//
+//  Created by Siddharth Kothari on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCodeGenerator/BarCodeGenerator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BarCodeGenerator
+//
+//  Created by Siddharth Kothari on 08/02/24.
+//
+
+// courtsey: https://www.appcoda.com/swiftui-barcode-generator/?ck_subscriber_id=790969036
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
