d019254969ca82485283d0567805aa3c71ce6fde
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-12 17:54:56 +0530

    ObservableVsObservableObject: ObservableAsState initial setup

---
+++ ObservableVsObservableObject/ObservableVsObservableObject/1.ObservableAsState.swift
@@ -0,0 +1,58 @@
+//
+//  1.ObservableAsState.swift
+//  ObservableVsObservableObject
+//
+//  Created by Siddharth Kothari on 12/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+// With ObservableObject, we need to use a specific property wrapper to tell the view “this object is a source of truth”. This allows SwiftUI to redraw your view when the object updates one of its @Published properties.
+
+// Note that the view won’t care which property changed. Any change to any @Published property will cause your view body to be re-evaluated (and redrawn) regardless of whether the object update results in a changed view.
+
+@Observable
+class Counter {
+  var currentValue: Int = 0
+}
+
+
+struct ObservableAsState: View {
+   // When we apply @State to your creation of an @Observable, we do this due to @State's ability to cache the instance it’s applied to across view redraws.
+    @State var id = UUID()
+    
+    var body: some View {
+        VStack {
+            Button("Change id") {
+                id = UUID()
+            }
+            Text("Current id: \(id)")
+            
+            ButtonView()
+        }.padding()
+    }
+}
+
+struct ButtonView: View {
+  let counter = Counter()
+
+  var body: some View {
+    VStack {
+      Text("Counter is tapped \(counter.currentValue) times")
+      Button("Increase") {
+        counter.currentValue += 1
+      }
+    }.padding()
+  }
+}
+
+/* When you run this code, you’ll find that tapping the Increase button works without any issues. The counter goes up and the view updates.
+
+However, when you tap on Change id the counter resets back to 0.
+
+That’s because once the ContentView redraws, a new instance of ButtonView is created which will also create a new Counter. */
+
+#Preview {
+    ObservableAsState()
+}
