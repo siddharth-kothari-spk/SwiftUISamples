105800284c0d06cbf301ab5301af93bbe7fddf3b
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-12 17:10:14 +0530

    ObservableVsObservableObject: courtsey link added

---
+++ ObservableVsObservableObject/ObservableVsObservableObject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ObservableVsObservableObject
+//
+//  Created by Siddharth Kothari on 12/02/24.
+//
+
+// courtsey: https://www.donnywals.com/comparing-observable-to-observableobjects/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableVsObservableObject/ObservableVsObservableObject/ObservableVsObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableVsObservableObjectApp.swift
+//  ObservableVsObservableObject
+//
+//  Created by Siddharth Kothari on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableVsObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
