9391641057f468035bde5a9ab3a1d1e1b12a7d3e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-18 11:34:09 +0530

    Popover: added pacjkage popovers

---
+++ Popover/Popover/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Popover
+//
+//  Created by Siddharth Kothari on 18/02/24.
+//
+
+import SwiftUI
+
+// courtsey: https://betterprogramming.pub/how-to-add-custom-popovers-to-your-swiftui-ios-app-814bdfad73d0
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Popover/Popover/PopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverApp.swift
+//  Popover
+//
+//  Created by Siddharth Kothari on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
