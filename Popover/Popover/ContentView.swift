661bc8ec65c4d710bc6b6802a345a6f9bc86066a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-18 19:18:27 +0530

    Popover: basic popover 3

--- Popover/Popover/ContentView.swift
+++ Popover/Popover/ContentView.swift
@@ -13,43 +13,71 @@ import Popovers
 struct ContentView: View {
     @State var basicPopoverPresent1: Bool = false
     @State var basicPopoverPresent2: Bool = false
+    @State var basicPopoverPresent3: Bool = false
     
     var body: some View {
         VStack {
-            HStack {
-                Button("Present popover 1") {
-                    basicPopoverPresent1 = true
-                }
-                .popover(present: $basicPopoverPresent1) {
-                    Text("Hi, I'm a popover.")
-                                    .padding()
-                                    .foregroundColor(.white)
-                                    .background(.blue)
-                                    .cornerRadius(16)
-                }
+            VStack {
+                Text("Basic Popovers").padding()
+                HStack {
+                    Button("Present popover 1") {
+                        basicPopoverPresent1 = true
+                    }
+                    .padding()
+                    .popover(present: $basicPopoverPresent1) {
+                        Text("Hi, I'm a popover.")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(16)
+                    }
+                    
+                    Button("Present popover 2") {
+                        basicPopoverPresent2 = true
+                    }
+                    .padding()
+                    .border(.green)
+                    .popover(
+                        present: $basicPopoverPresent2,
+                        attributes: {
+                            $0.position = .absolute(
+                                originAnchor: .top,
+                                popoverAnchor: .bottom
+                            )
+                            $0.sourceFrameInset.top = -16
+                        }
+                    ) {
+                        Text("Hi, I'm a popover that's attached to the button's top, with a bit of spacing.")
+                            .frame(maxWidth: 250)
+                            .padding()
+                            .background(.regularMaterial)
+                            .border(.blue)
+                    }
+                }.padding()
                 
-                Button("Present popover 2") {
-                    basicPopoverPresent2 = true
+                Button("Present popover 3") {
+                    basicPopoverPresent3 = true
                 }
                 .padding()
-                .border(.green)
                 .popover(
-                    present: $basicPopoverPresent2,
+                    present: $basicPopoverPresent3,
                     attributes: {
-                        $0.position = .absolute(
-                            originAnchor: .top,
-                            popoverAnchor: .bottom
-                        )
-                        $0.sourceFrameInset.top = -16
+                        $0.presentation.animation = .spring(response: 0.6, dampingFraction: 0.4, blendDuration: 1)
+                        $0.presentation.transition = .offset(x: 0, y: 30).combined(with: .opacity)
+                        $0.dismissal.transition = .offset(x: 0, y: 30).combined(with: .opacity)
+                        $0.rubberBandingMode = .yAxis
                     }
                 ) {
-                    Text("Hi, I'm a popover that's attached to the button's top, with a bit of spacing.")
-                        .frame(maxWidth: 250)
+                    Text("Hi, I'm a bouncy popover!")
                         .padding()
-                        .background(.regularMaterial)
-                        .border(.blue)
+                        .foregroundColor(.white)
+                        .background(
+                            LinearGradient(colors: [.blue, .green], startPoint: .top, endPoint: .bottom),
+                            in: RoundedRectangle(cornerRadius: 16)
+                        )
                 }
-            }
+            
+        }
         }
         .padding()
     }
