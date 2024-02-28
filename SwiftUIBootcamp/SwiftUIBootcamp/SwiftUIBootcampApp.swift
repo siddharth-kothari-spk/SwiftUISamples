481199d0a5bd9322e6b787d0ad574518f4626922
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-28 15:58:19 +0530

    SwiftUIBootcamp: initial commit

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Siddharth Kothari on 28/02/24.
+//
+
+// courtsey: https://www.youtube.com/watch?v=TQ9f8I8bQC8&t=159s&ab_channel=AppStuff
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
