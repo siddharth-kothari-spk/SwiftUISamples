13abac1880da2571652ec0babb93b8159737352c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-29 17:29:29 +0530

    SwiftUIBootcamp: StacksModule- challenge

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/Stacks/StackChallenge.swift
@@ -0,0 +1,112 @@
+//
+//  StackChallenge.swift
+//  SwiftUIBootcamp
+//
+//  Created by Siddharth Kothari on 29/02/24.
+//
+
+import SwiftUI
+
+struct StackChallenge: View {
+    var body: some View {
+        VStack(content: {
+            Text("without divider")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            VStack(alignment: .leading) {
+                // alignment is leading here but items do not move to left as the width is limited by HStack contents, but Divider() goes across the screen
+                HStack {
+                    Image("photo2")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, content: {
+                        Text("headline text")
+                            //.frame(width: 100, height: 40)
+                            .font(.headline)
+                            .foregroundStyle(.blue)
+                        
+                        Text("Sub headline test")
+                           // .frame(width: 100, height: 40)
+                            .font(.subheadline)
+                            .foregroundStyle(.green)
+                    })
+                }
+                
+                HStack {
+                    Image("photo2")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, content: {
+                        Text("headline text")
+                            //.frame(width: 100, height: 40)
+                            .font(.headline)
+                            .foregroundStyle(.blue)
+                        
+                        Text("Sub headline test")
+                           // .frame(width: 100, height: 40)
+                            .font(.subheadline)
+                            .foregroundStyle(.green)
+                    })
+                }
+            }
+            
+            
+            Text("with divider")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("photo2")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, content: {
+                        Text("headline text")
+                            //.frame(width: 100, height: 40)
+                            .font(.headline)
+                            .foregroundStyle(.blue)
+                        
+                        Text("Sub headline test")
+                           // .frame(width: 100, height: 40)
+                            .font(.subheadline)
+                            .foregroundStyle(.green)
+                    })
+                }
+                Divider()
+                
+                HStack {
+                    Image("photo2")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, content: {
+                        Text("headline text")
+                            //.frame(width: 100, height: 40)
+                            .font(.headline)
+                            .foregroundStyle(.blue)
+                        
+                        Text("Sub headline test")
+                           // .frame(width: 100, height: 40)
+                            .font(.subheadline)
+                            .foregroundStyle(.green)
+                    })
+                }
+                Divider()
+
+            }
+        })
+
+    }
+}
+
+#Preview {
+    StackChallenge()
+}
