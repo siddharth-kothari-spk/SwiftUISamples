0bc31848c1ebf4cb666c0bb150c3da3ef26b05c0
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-29 15:48:25 +0530

    SwiftUIBootcamp: StacksModule- refactoring

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/Stacks/StackBasic.swift
@@ -0,0 +1,18 @@
+//
+//  StackBasic.swift
+//  SwiftUIBootcamp
+//
+//  Created by Siddharth Kothari on 29/02/24.
+//
+
+import SwiftUI
+
+struct StackBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StackBasic()
+}
--- SwiftUIBootcamp/SwiftUIBootcamp/Stacks/StacksModule.swift
+++ SwiftUIBootcamp/SwiftUIBootcamp/Stacks/StacksModule.swift
@@ -9,61 +9,7 @@ import SwiftUI
 
 struct StacksModule: View {
     var body: some View {
-        VStack {
-            
-            Spacer()
-            
-            Rectangle()
-                .fill(.red)
-                .frame(width: 200, height: 100)
-                .clipShape(Circle(), style: FillStyle(eoFill: true))
-            
-            Capsule(style: .continuous)
-                .fill(.green)
-                .frame(width: 200,height: 50)
-            
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 200, height: 100)
-                .clipShape(Circle(), style: FillStyle(antialiased: false))
-            
-            
-            Spacer()
-            
-            HStack {
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 200, height: 100)
-                    .clipShape(Circle(), style: FillStyle(eoFill: true))
-                
-                Capsule(style: .continuous)
-                    .fill(.green)
-                    .frame(width: 50,height: 200)
-                Rectangle()
-                    .fill(.blue)
-                    .frame(width: 200, height: 100)
-                    .clipShape(Circle(), style: FillStyle(antialiased: false))
-            }.padding()
-            
-            Spacer()
-            
-            ZStack(content: {
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle(), style: FillStyle(eoFill: true))
-                
-                Rectangle()
-                    .fill(.blue)
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle(), style: FillStyle(eoFill: true))
-                
-                Rectangle()
-                    .fill(.yellow)
-                    .frame(width: 50, height: 50)
-                    .clipShape(Circle(), style: FillStyle(eoFill: true))
-            }).padding()
-        }.padding()
+       StackBasic()
     }
 }
 
