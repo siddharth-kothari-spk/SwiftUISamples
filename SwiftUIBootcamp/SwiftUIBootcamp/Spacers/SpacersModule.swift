495b603aba5d3031e6b2640b0f5cc50397c90a02
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-01 23:17:16 +0530

    SwiftUIBootcamp: SpacersModule: usgae in HStack

--- SwiftUIBootcamp/SwiftUIBootcamp/Spacers/SpacersModule.swift
+++ SwiftUIBootcamp/SwiftUIBootcamp/Spacers/SpacersModule.swift
@@ -10,15 +10,22 @@ import SwiftUI
 struct SpacersModule: View {
     var body: some View {
         VStack {
-            Spacer() // Spacer fill all the space vertically by default
+            Spacer() // Spacer fill all the space vertically in VStack
                 .frame(width: 50)
             .background(.red)
             
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Vertical")
             
             Spacer()
                 .frame(width: 50)
                 .background(.red)
+            
+            HStack(content: {
+                Text("Horizontal")
+                Spacer() // Spacer fill all the space horizontally in HStack
+                    .frame(height: 25)
+                    .background(.cyan)
+            })
         }
     }
 }
