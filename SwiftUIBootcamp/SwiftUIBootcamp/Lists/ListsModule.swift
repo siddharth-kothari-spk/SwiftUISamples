42f5d1d14e268bb510b427b9164c4c8911e5d732
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-03 08:17:56 +0530

    SwiftUIBootcamp: ListsModule- using sections

--- SwiftUIBootcamp/SwiftUIBootcamp/Lists/ListsModule.swift
+++ SwiftUIBootcamp/SwiftUIBootcamp/Lists/ListsModule.swift
@@ -7,11 +7,40 @@
 
 import SwiftUI
 
+let foods : [String] = [
+    "food1",
+    "food2",
+    "food3",
+    "food4",
+    "food5",
+    "food6",
+    "food7",
+    "food8",
+    "food9",
+    "food10"]
+
 struct ListsModule: View {
     var body: some View {
         List {
-            ForEach(0...100, id: \.self) { index in
-                Text("Index \(index)")
+            Section {
+                ForEach(0...5, id: \.self) { index in
+                    Text("Index \(index)")
+                }
+            }
+            
+            Section {
+                ForEach(foods, id: \.self) { food in
+                    Text(food)
+                }
+            }
+            
+            Section {
+                ForEach(foods, id: \.self) { food in
+                    Image(food)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 360, height: 200)
+                }
             }
         }
     }
