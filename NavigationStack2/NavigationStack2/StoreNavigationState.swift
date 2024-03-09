9aac4e8ffad1b6f02bcd072855dcc41fa65eb5c8
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-09 15:32:04 +0530

    NavigationStack2: StoreNavigationState

---
+++ NavigationStack2/NavigationStack2/StoreNavigationState.swift
@@ -0,0 +1,61 @@
+//
+//  StoreNavigationState.swift
+//  NavigationStack2
+//
+//  Created by Siddharth Kothari on 09/03/24.
+//
+
+import SwiftUI
+
+struct StoreNavigationState: View {
+    private var bgColors: [Color] = [ .indigo, .yellow, .green, .orange, .brown ]
+ 
+    @State private var path: [Color] = []
+ 
+    var body: some View {
+ 
+        NavigationStack(path: $path) {
+            List(bgColors, id: \.self) { bgColor in
+ 
+                NavigationLink(value: bgColor) {
+                    Text(bgColor.description)
+                }
+ 
+            }
+            .listStyle(.plain)
+ 
+            .navigationDestination(for: Color.self) { color in
+                VStack {
+                    Text("\(path.count), \(path.description)")
+                        .font(.headline)
+ 
+                    HStack {
+                        ForEach(path, id: \.self) { color in
+                            color
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+ 
+                    }
+ 
+                    List(bgColors, id: \.self) { bgColor in
+ 
+                        NavigationLink(value: bgColor) {
+                            Text(bgColor.description)
+                        }
+ 
+                    }
+                    .listStyle(.plain)
+ 
+                }
+            }
+ 
+            .navigationTitle("Color")
+ 
+        }
+ 
+    }
+}
+
+#Preview {
+    StoreNavigationState()
+}
