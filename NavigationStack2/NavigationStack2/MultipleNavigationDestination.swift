aa51bcafc3d1701e22ced6628cd6af95bfd8815e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-09 08:21:33 +0530

    NavigationStack2: MultipleNavigationDestination

---
+++ NavigationStack2/NavigationStack2/MultipleNavigationDestination.swift
@@ -0,0 +1,44 @@
+//
+//  MultipleNavigationDestination.swift
+//  NavigationStack2
+//
+//  Created by Siddharth Kothari on 09/03/24.
+//
+
+import SwiftUI
+
+struct MultipleNavigationDestination: View {
+    private var bgColors: [Color] = [.red, .green, .blue, .yellow, .purple, .black, .orange, .pink, .cyan]
+    private var systemImages: [String] = [ "trash", "cloud", "bolt" ]
+    var body: some View {
+        NavigationStack {
+            List(bgColors, id: \.self) { bgColor in
+                NavigationLink(value: bgColor) {
+                    Text(bgColor.description)
+                }
+            }
+            
+            List(systemImages,id:\.self, rowContent: { systemImage in
+                NavigationLink(value: systemImage) {
+                    Image(systemName: systemImage)
+                        .font(.system(size: 40))
+                }
+                
+            })
+            .navigationDestination(for: Color.self) { color in
+                color
+                    .frame(maxWidth: .infinity,
+                           maxHeight: .infinity)
+            }
+            .navigationDestination(for: String.self, destination: { image in
+                Image(systemName: image)
+                    .font(.system(size: 100))
+            })
+            .navigationTitle("Navigate")
+        }
+    }
+}
+
+#Preview {
+    MultipleNavigationDestination()
+}
