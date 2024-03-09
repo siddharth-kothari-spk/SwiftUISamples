e4374615605ff5d549d492495d6927d5d61a302a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-09 07:16:04 +0530

    NavigationStack2: initial commit

---
+++ NavigationStack2/NavigationStack2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NavigationStack2
+//
+//  Created by Siddharth Kothari on 09/03/24.
+//
+
+import SwiftUI
+// https://www.appcoda.com/navigationstack/
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationStack2/NavigationStack2/NavigationStack2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack2App.swift
+//  NavigationStack2
+//
+//  Created by Siddharth Kothari on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
