ee6d15adac38deff7a1920d53f9f517fcfd31d07
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-09 08:10:46 +0530

    NavigationStack2: MasterDetailFlow - style 2

--- NavigationStack2/NavigationStack2/MasterDetailFlow.swift
+++ NavigationStack2/NavigationStack2/MasterDetailFlow.swift
@@ -11,6 +11,8 @@ struct MasterDetailFlow: View {
   private var bgColors: [Color] = [.red, .green, .blue, .yellow, .purple, .black, .orange, .pink, .cyan]
     
     var body: some View {
+        // style 1
+        /*
         NavigationStack {
             List(bgColors, id: \.self) { bgColor in
                 NavigationLink(bgColor.description) {
@@ -21,6 +23,21 @@ struct MasterDetailFlow: View {
             }
             .navigationTitle("Color")
             .listStyle(.insetGrouped)
+        }
+        */
+        // style 2
+        NavigationStack {
+            List(bgColors, id: \.self) { bgColor in
+                NavigationLink(value: bgColor) {
+                    Text(bgColor.description)
+                }
+            }
+            .navigationDestination(for: Color.self) { color in
+                color
+                    .frame(maxWidth: .infinity,
+                           maxHeight: .infinity)
+            }
+            .navigationTitle("Color")
         }
     }
 }
