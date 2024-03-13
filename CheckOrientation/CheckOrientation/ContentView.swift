86eb508b6bcf19a52aa090a2ca5811a3125f7981
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-13 11:53:39 +0530

    CheckOrientation: demonstration

--- CheckOrientation/CheckOrientation/ContentView.swift
+++ CheckOrientation/CheckOrientation/ContentView.swift
@@ -10,14 +10,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var orientation = UIDeviceOrientation.unknown
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            if orientation.isPortrait {
+                    Text("Portrait")
+                } else if orientation.isLandscape {
+                    Text("Landscape")
+                } else if orientation.isFlat {
+                    Text("Flat")
+                } else {
+                    Text("Unknown")
+                }
         }
-        .padding()
+        .onRotate { newOrientation in
+            orientation = newOrientation
+        }
     }
 }
 
