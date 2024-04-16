146afffb19d44c02424fd24afea2cdc6d8190fbd
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-16 13:36:17 +0530

    Custom_Property_Wrapper: initial commit

---
+++ Custom_Property_Wrapper/Custom_Property_Wrapper/Custom_Property_WrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Property_WrapperApp.swift
+//  Custom_Property_Wrapper
+//
+//  Created by Siddharth Kothari on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Property_WrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Custom_Property_Wrapper/Custom_Property_WrapperUITests/Custom_Property_WrapperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Custom_Property_WrapperUITestsLaunchTests.swift
+//  Custom_Property_WrapperUITests
+//
+//  Created by Siddharth Kothari on 16/04/24.
+//
+
+import XCTest
+
+final class Custom_Property_WrapperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
