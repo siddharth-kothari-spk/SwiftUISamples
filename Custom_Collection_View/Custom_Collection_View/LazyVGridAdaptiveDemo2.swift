7e955417cec85dacf26882e5d0b36f5ae5dfc9d1
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-04 12:33:50 +0530

    Custom_Collection_View: LazyVGridAdaptiveDemo2

---
+++ Custom_Collection_View/Custom_Collection_View/LazyVGridAdaptiveDemo2.swift
@@ -0,0 +1,33 @@
+//
+//  LazyVGridAdaptiveDemo2.swift
+//  Custom_Collection_View
+//
+//  Created by Siddharth Kothari on 04/05/24.
+//
+
+import SwiftUI
+
+struct LazyVGridAdaptiveDemo2: View {
+    var body: some View {
+        LazyVGrid(
+            columns: [GridItem(.adaptive(minimum: 10, maximum: 200))],
+            // This is because adaptive means inserting as many items of the minimum size as possible at least in SwiftUI world
+            alignment: .center) {
+                ForEach(0..<7) {index in
+                    Text("item \(index)")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 16)
+                        .lineLimit(1)
+                        .background(RoundedRectangle(cornerRadius: 16))
+                }
+            }
+            .padding(.all, 50)
+            .background(Color(UIColor.lightGray))
+            
+    }
+}
+
+#Preview {
+    LazyVGridAdaptiveDemo2()
+}
