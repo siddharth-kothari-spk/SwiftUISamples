9cdb2d3438d4dd3b92ccf339c56799f4492295dc
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-04 12:46:43 +0530

    Custom_Collection_View: CustomCollectionView usage

--- Custom_Collection_View/Custom_Collection_View/ContentView.swift
+++ Custom_Collection_View/Custom_Collection_View/ContentView.swift
@@ -9,14 +9,12 @@
 // Challenge: A CollectionView with dynamic number of columns (may differ by row) depending on the item width.
 
 import SwiftUI
+let items: [String] = ["item 1", "item 2 - long long long long long long", "item 3", "item 4", "item 5", "item 6", "item 7 - medium", "item 8"]
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CustomCollectionView(items: items)
         }
         .padding()
     }
