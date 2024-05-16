62da2d4b17202419543cd641ecaee8c01332c9ae
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-16 16:41:55 +0530

    Chat_Interface: initial commit

---
+++ Chat_Interface/Chat_Interface/Chat_InterfaceApp.swift
@@ -0,0 +1,19 @@
+//
+//  Chat_InterfaceApp.swift
+//  Chat_Interface
+//
+//  Created by Siddharth Kothari on 16/05/24.
+//
+
+import SwiftUI
+
+// courtsey: https://alep.medium.com/creating-a-chat-interface-with-swiftui-30ca977456d6
+
+@main
+struct Chat_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat_Interface/Chat_InterfaceUITests/Chat_InterfaceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chat_InterfaceUITestsLaunchTests.swift
+//  Chat_InterfaceUITests
+//
+//  Created by Siddharth Kothari on 16/05/24.
+//
+
+import XCTest
+
+final class Chat_InterfaceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
