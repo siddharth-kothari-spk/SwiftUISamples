1cd12484cd139041d475abea78a3445fd3a7804f
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-19 07:39:04 +0530

    Test_JLLT: initial commit

---
+++ Test_JLLT/Test_JLLT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_JLLT
+//
+//  Created by Siddharth Kothari on 19/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_JLLT/Test_JLLT/Test_JLLTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_JLLTApp.swift
+//  Test_JLLT
+//
+//  Created by Siddharth Kothari on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_JLLTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
