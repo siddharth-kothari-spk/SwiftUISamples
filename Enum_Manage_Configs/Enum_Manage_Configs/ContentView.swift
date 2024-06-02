1368fb65f67c41d3e2308094d4d05068b8327f3c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-06-02 08:03:03 +0530

    Enum_Manage_Configs: Config View used

--- Enum_Manage_Configs/Enum_Manage_Configs/ContentView.swift
+++ Enum_Manage_Configs/Enum_Manage_Configs/ContentView.swift
@@ -11,13 +11,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        EnumConfigView()
     }
 }
 
