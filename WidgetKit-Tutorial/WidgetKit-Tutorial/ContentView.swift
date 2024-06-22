d64a4a9d1e8964c6bbe51cf88bbe86185a65bc3e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-06-22 07:13:51 +0530

    WidgetKit-Tutorial: initial commit

---
+++ WidgetKit-Tutorial/WidgetKit-Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetKit-Tutorial
+//
+//  Created by Siddharth Kothari on 22/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetKit-Tutorial/WidgetKit-Tutorial/WidgetKit_TutorialApp.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetKit_TutorialApp.swift
+//  WidgetKit-Tutorial
+//
+//  Created by Siddharth Kothari on 22/06/24.
+//
+
+// courtsey: https://www.createwithswift.com/adding-a-widget-to-a-swiftui-app/
+import SwiftUI
+
+@main
+struct WidgetKit_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
