2f8d17c2e814197d197ff98a73bde9aa360b649f
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-23 18:35:39 +0530

    WeatherDemo: initial commit

---
+++ WeatherDemo/WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Siddharth Kothari on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
