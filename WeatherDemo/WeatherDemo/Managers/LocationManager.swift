768fa60fa51292b13f8b519bce29c879a54ea1df
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-26 07:15:59 +0530

    WeatherDemo: added Location Manager class

---
+++ WeatherDemo/WeatherDemo/Managers/LocationManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocationManager.swift
+//  WeatherDemo
+//
+//  Created by Siddharth Kothari on 23/07/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D? // need as optional if user does not gives permission
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+}
