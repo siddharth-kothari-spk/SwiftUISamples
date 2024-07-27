8c894fa71d8cf3d1d685f3cfefdd6ea8b0c939ea
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-27 07:23:21 +0530

    WeatherDemo: added StateObject in ContentView

--- WeatherDemo/WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/WeatherDemo/Views/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
     var body: some View {
         VStack {
             Image(systemName: "globe")
