830ab136a69497736890fbda1844de4ba9e8f611
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-28 07:03:39 +0530

    WeatherDemo: added Utilities and initiated api call

--- WeatherDemo/WeatherDemo/Managers/WeatherManager.swift
+++ WeatherDemo/WeatherDemo/Managers/WeatherManager.swift
@@ -10,9 +10,18 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid={API key}")  else {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(Utilities.apiKey)&units=metric")  else {
             fatalError("Missing url")
         }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            fatalError("Error fetching weather data")
+        }
+        
         
     }
 }
---
+++ WeatherDemo/WeatherDemo/Utilities.swift
@@ -0,0 +1,12 @@
+//
+//  Utilities.swift
+//  WeatherDemo
+//
+//  Created by Siddharth Kothari on 28/07/24.
+//
+
+import Foundation
+
+class Utilities {
+    static let apiKey = "0dbaeced0a5df7db7e340c4b3b3794d5"
+}
