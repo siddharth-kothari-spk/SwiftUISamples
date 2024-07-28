e74c39929975ab385584faf65125e15560370c93
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-28 19:17:56 +0530

    WeatherDemo: double extension added

---
+++ WeatherDemo/WeatherDemo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherDemo
+//
+//  Created by Siddharth Kothari on 28/07/24.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%0.f", self)
+    }
+}
