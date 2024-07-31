8f1674b13967ba67a2476ef89a933152d51a2ad1
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-31 07:34:32 +0530

    WeatherDemo: added drizzle weather codes

--- WeatherDemo/WeatherDemo/Enums/WeatherCodes.swift
+++ WeatherDemo/WeatherDemo/Enums/WeatherCodes.swift
@@ -22,6 +22,17 @@ enum WeatherCodes: Int {
     case thunderstorm_with_drizzle = 231
     case thunderstorm_with_heavy_drizzle = 232
     
+    // 3xx series: drizzle
+    case light_intensity_drizzle = 300
+    case drizzle = 301
+    case heavy_intensity_drizzle = 302
+    case light_intensity_drizzle_rain = 310
+    case drizzle_rain = 311
+    case heavy_intensity_drizzle_rain = 312
+    case shower_rain_and_drizzle = 313
+    case heavy_shower_rain_and_drizzle = 314
+    case shower_drizzle = 321
+    
     // clouds
     case scattered_clouds_25_50_percent = 802
     
@@ -59,6 +70,35 @@ enum WeatherCodes: Int {
             
         case .thunderstorm_with_heavy_drizzle:
             return "thunderstorm with heavy drizzle"
+            
+        
+        // drizzle
+        case .drizzle:
+            return "drizzle"
+            
+        case .drizzle_rain:
+            return "drizzle rain"
+            
+        case .shower_drizzle:
+            return "shower drizzle"
+            
+        case .heavy_intensity_drizzle:
+            return "heavy intensity drizzle"
+            
+        case .light_intensity_drizzle:
+            return "light intensity drizzle"
+            
+        case .shower_rain_and_drizzle:
+            return "shower rain and drizzle"
+            
+        case .heavy_intensity_drizzle_rain:
+            return "heavy intensity drizzle rain"
+            
+        case .light_intensity_drizzle_rain:
+            return "light intensity drizzle rain"
+            
+        case .heavy_shower_rain_and_drizzle:
+            return "heavy shower rain and drizzle"
          
         // clounds
         case .scattered_clouds_25_50_percent:
