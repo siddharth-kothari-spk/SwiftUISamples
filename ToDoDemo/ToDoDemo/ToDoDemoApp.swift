b452e75eb0d580cd178981511e8121eb2e8dbdbc
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-02 14:35:50 +0530

    ToDoDemo: initial commit

---
+++ ToDoDemo/ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Siddharth Kothari on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
