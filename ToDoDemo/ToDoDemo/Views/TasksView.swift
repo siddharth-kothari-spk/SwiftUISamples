75331ad5e880564813ea9dd138b3f42948d01436
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-04 17:32:33 +0530

    ToDoDemo: update and delete task put on list

--- ToDoDemo/ToDoDemo/Views/TasksView.swift
+++ ToDoDemo/ToDoDemo/Views/TasksView.swift
@@ -18,16 +18,28 @@ struct TasksView: View {
             
             List {
                 ForEach(realmManager.tasks, id: \.id) { task in
-                    TaskRow(task: task.title, completed: task.completed)
-                        .onTapGesture {
-                            realmManager.updateTask(id: task.id, completed: !task.completed)
-                        }
+                    if !task.isInvalidated {
+                        TaskRow(task: task.title, completed: task.completed)
+                            .onTapGesture {
+                                realmManager.updateTask(id: task.id, completed: !task.completed)
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button(role: .destructive) {
+                                    realmManager.deletetask(id: task.id)
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                            }
+                    }
+                    
                 }
             }
             .onAppear(perform: {
                 UITableView.appearance().backgroundColor = UIColor.clear
                 UITableViewCell.appearance().backgroundColor = UIColor.clear
             })
+            
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Utilities.backgroundColor())
