9579c9f84d125ee258e70e833a317e0940abeca5
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-05 07:19:13 +0530

    ToDoDemo: 2nd way to delete

--- ToDoDemo/ToDoDemo/Views/TasksView.swift
+++ ToDoDemo/ToDoDemo/Views/TasksView.swift
@@ -17,22 +17,28 @@ struct TasksView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
             
             List {
-                ForEach(realmManager.tasks, id: \.id) { task in
+                ForEach(realmManager.tasks, id: \.self) { task in
                     if !task.isInvalidated {
                         TaskRow(task: task.title, completed: task.completed)
                             .onTapGesture {
                                 realmManager.updateTask(id: task.id, completed: !task.completed)
                             }
-                            .swipeActions(edge: .trailing) {
-                                Button(role: .destructive) {
-                                    realmManager.deletetask(id: task.id)
-                                } label: {
-                                    Label("Delete", systemImage: "trash")
-                                }
-                            }
+//                            .swipeActions(edge: .trailing) {
+//                                Button(role: .destructive) {
+//                                    realmManager.deletetask(id: task.id)
+//                                } label: {
+//                                    Label("Delete", systemImage: "trash")
+//                                }
+//                            }
                     }
                     
                 }
+                .onDelete(perform: { indexSet in
+                    for index in indexSet {
+                        let task = realmManager.tasks[index]
+                        realmManager.deletetask(id: task.id)
+                    }
+                })
             }
             .onAppear(perform: {
                 UITableView.appearance().backgroundColor = UIColor.clear
