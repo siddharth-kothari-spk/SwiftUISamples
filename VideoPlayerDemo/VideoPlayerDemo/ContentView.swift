10cb28e704ce747ba5e89e166d89ed489fdbfbbc
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-08 07:56:06 +0530

    VideoPlayerDemo: initial commit

---
+++ VideoPlayerDemo/VideoPlayerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoPlayerDemo
+//
+//  Created by Siddharth Kothari on 08/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoPlayerDemo/VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by Siddharth Kothari on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
