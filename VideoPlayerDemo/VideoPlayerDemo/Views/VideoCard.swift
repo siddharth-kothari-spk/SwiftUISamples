2d546e4198d3c38b876fb71d5785d59d3bce1256
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-09 08:06:13 +0530

    VideoPlayerDemo: added VideoCard view

---
+++ VideoPlayerDemo/VideoPlayerDemo/Views/VideoCard.swift
@@ -0,0 +1,54 @@
+//
+//  VideoCard.swift
+//  VideoPlayerDemo
+//
+//  Created by Siddharth Kothari on 09/08/24.
+//
+
+import SwiftUI
+
+struct VideoCard: View {
+    var body: some View {
+        ZStack {
+            ZStack(alignment: .bottomLeading, content: {
+                AsyncImage(url: URL(string: "")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 250)
+                        .clipShape(.buttonBorder)
+                    
+                } placeholder: {
+                    Rectangle()
+                        .foregroundStyle(.gray.opacity(0.3))
+                        .frame(width: 150, height: 250)
+                        .clipShape(.buttonBorder)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("50 sec")
+                        .font(.caption.bold())
+                    
+                    Text("By XYZ")
+                        .font(.caption.bold())
+                        .multilineTextAlignment(.leading) // so if content goes in more than 1 line it does not shift to center
+                }
+                .padding()
+                .foregroundStyle(.white)
+                .shadow(color:.black, radius: 10)
+                
+            })
+            
+            Image(systemName: "play.fill")
+                .foregroundStyle(.white)
+                .font(.title)
+                .padding()
+                .background(.thinMaterial)
+                .clipShape(Circle())
+        }
+    }
+}
+
+#Preview {
+    VideoCard()
+}
