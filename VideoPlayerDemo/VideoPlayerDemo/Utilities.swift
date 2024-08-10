899865501f7ac2d18400ea1ba5a75b301b302d4e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-10 13:18:11 +0530

    VideoPlayerDemo: added Utilities to access api key

---
+++ VideoPlayerDemo/VideoPlayerDemo/Utilities.swift
@@ -0,0 +1,14 @@
+//
+//  Utilities.swift
+//  VideoPlayerDemo
+//
+//  Created by Siddharth Kothari on 10/08/24.
+//
+
+import Foundation
+
+class Utilities {
+    static func apiKey() -> String {
+        return ""
+    }
+}
