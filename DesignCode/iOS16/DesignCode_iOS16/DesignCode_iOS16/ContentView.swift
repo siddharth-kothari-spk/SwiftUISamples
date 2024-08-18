22eda61bfc31e69cff6c3176c66cab1eeb4e5bad
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-08-18 18:04:29 +0530

    DesignCode_iOS16: changing views from visual editor

--- DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ContentView.swift
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .leading, spacing: 20.0) {
+            Image(systemName: "cellularbars")
+                .foregroundColor(Color("AccentColor"))
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
             Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Drag and drop the view for multiline text with some random data")
+                .multilineTextAlignment(.center)
+            Button("Test button") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+            .frame(width: 300.0)
+            .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                
         }
-        .padding()
+        .padding(.all)
     }
 }
 
