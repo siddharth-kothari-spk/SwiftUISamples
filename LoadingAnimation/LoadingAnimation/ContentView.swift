ce24e0bbbf482beacd620e72f58dd379b3060843
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-01 11:36:19 +0530

    LoadingAnimation: initial commit, courtsey link

---
+++ LoadingAnimation/LoadingAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LoadingAnimation
+//
+//  Created by Siddharth Kothari on 01/09/24.
+//
+
+// courtsey: https://x.com/sucodeee/status/1819715043241410982
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadingAnimation/LoadingAnimation/LoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAnimationApp.swift
+//  LoadingAnimation
+//
+//  Created by Siddharth Kothari on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
