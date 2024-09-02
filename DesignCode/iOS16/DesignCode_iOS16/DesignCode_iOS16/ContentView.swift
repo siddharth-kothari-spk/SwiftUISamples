83927a5470665ab44a0398efad5f84819910e9de
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-02 06:20:11 +0530

    DesignCode_iOS16: 1. re arranging code in custom views internally

--- DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ContentView.swift
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ContentView.swift
@@ -13,15 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20.0) {
-            Image(systemName: "cellularbars", variableValue: time)
-                .foregroundColor(Color("AccentColor"))
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                .font(.system(size: 50))
-                .fontWeight(.thin)
-                .onReceive(timer, perform: { _ in
-                    time = (time + 0.25).truncatingRemainder(dividingBy: 4.0)
-                })
+            sfIcon
             
             Text("Hello, world!".localizedCapitalized)
                 .font(.largeTitle.width(.condensed)) // A width to use for fonts that have multiple widths
@@ -32,36 +24,8 @@ struct ContentView: View {
                 .foregroundColor(.secondary) // for vibrancy (Glossary A)
                 .fontWeight(.medium)
             
-            // Currently button is only tappable where text is present but not on entire width. To resolve it we will use different variation of Button where we can add Text view inside it and then apply all modifiers on it.
-            
-            // Before:
-            /*
-             Button("Test button") {
-                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-             }
-             .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-             .frame(width: 300.0)
-             .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-             */
-            
-            // After:
-            Button(action: {
-                
-            }, label: {
-                Text("Test Button")
-                    .padding(.all, 10.0)
-                    .frame(maxWidth: .infinity) // done so that fix width doesn't create issue on diff devices like watch and iPad
-                    //.border(Color("AccentColor"), width: 1)
-                    .background(.blue.opacity(0.8).gradient)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10) // this covers whole background
-                            .stroke(lineWidth: 5.0)
-                           // .foregroundColor(.secondary) // for vibrancy (Glossary A)
-                            .foregroundStyle(.linearGradient(colors: [.red.opacity(0.5), .yellow.opacity(0.5), .red.opacity(0.5), .yellow.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing)) // gradient
-                    )
-            })
-            .tint(.primary) // applies color to all elements inside button
-           // .shadow(color: .green, radius: 10) // adding contrast by shadow. shadow might not be properly visibile due to view background opacity
+           customButton
+           
         }
         .padding(30)
         .background(.ultraThinMaterial) // check Material enum
@@ -76,6 +40,52 @@ struct ContentView: View {
         .frame(maxWidth: 500)
         .padding(10)
         .dynamicTypeSize(.xSmall ... .xxxLarge)
+    }
+    
+    var sfIcon: some View {
+        Image(systemName: "cellularbars", variableValue: time)
+            .foregroundColor(Color("AccentColor"))
+            .imageScale(.large)
+            .foregroundStyle(.tint)
+            .font(.system(size: 50))
+            .fontWeight(.thin)
+            .onReceive(timer, perform: { _ in
+                time = (time + 0.25).truncatingRemainder(dividingBy: 4.0)
+            })
+    }
+    
+    var customButton: some View {
+        
+        // Currently button is only tappable where text is present but not on entire width. To resolve it we will use different variation of Button where we can add Text view inside it and then apply all modifiers on it.
+        
+        // Before:
+        /*
+         Button("Test button") {
+                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+         }
+         .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+         .frame(width: 300.0)
+         .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+         */
+        
+        // After:
+        Button(action: {
+            
+        }, label: {
+            Text("Test Button")
+                .padding(.all, 10.0)
+                .frame(maxWidth: .infinity) // done so that fix width doesn't create issue on diff devices like watch and iPad
+                //.border(Color("AccentColor"), width: 1)
+                .background(.blue.opacity(0.8).gradient)
+                .background(
+                    RoundedRectangle(cornerRadius: 10) // this covers whole background
+                        .stroke(lineWidth: 5.0)
+                       // .foregroundColor(.secondary) // for vibrancy (Glossary A)
+                        .foregroundStyle(.linearGradient(colors: [.red.opacity(0.5), .yellow.opacity(0.5), .red.opacity(0.5), .yellow.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing)) // gradient
+                )
+        })
+        .tint(.primary) // applies color to all elements inside button
+       // .shadow(color: .green, radius: 10) // adding contrast by shadow. shadow might not be properly visibile due to view background opacity
     }
 }
 
