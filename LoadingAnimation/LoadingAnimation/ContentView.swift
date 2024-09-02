41c73829aa282c48c8c6c01a2f2ad0d5da12f1b7
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-02 05:50:40 +0530

    LoadingAnimation: added bounce animation sample

--- LoadingAnimation/LoadingAnimation/ContentView.swift
+++ LoadingAnimation/LoadingAnimation/ContentView.swift
@@ -13,22 +13,43 @@ struct ContentView: View {
     @State var currentIndex = 0
     let colours: [Color] = [.orange, .blue, .red, .cyan, .yellow, .green]
     var body: some View {
-        HStack(spacing: 6, content: {
-            ForEach(0..<colours.count) { index in
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(colours[index])
-                    .frame(width: 5, height: currentIndex == index ? 100 : 5 )
-            }
-            .animation(.spring(duration: 0.9), value: currentIndex)
-            // check extension Animation for more options
+        VStack {
+            HStack(spacing: 6, content: {
+                ForEach(0..<colours.count) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(colours[index])
+                        .frame(width: 5, height: currentIndex == index ? 100 : 5 )
+                }
+                .animation(.spring(duration: 0.9), value: currentIndex)
+                // check extension Animation for more options
+            })
+            .onAppear(perform: {
+                startAnimation()
         })
-        .onAppear(perform: {
-            startAnimation()
+            
+            HStack(spacing: 6, content: {
+                ForEach(0..<colours.count) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(colours[index])
+                        .frame(width: 5, height: currentIndex == index ? 100 : 5 )
+                }
+                .animation(.bouncy(duration: 0.15, extraBounce: 0.15), value: currentIndex)
+                // check extension Animation for more options
+            })
+            .onAppear(perform: {
+                startAnimation2()
         })
+        }
     }
     
     func startAnimation() {
         Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true) { timer in
+            currentIndex = (currentIndex + 1) % colours.count
+        }
+    }
+    
+    func startAnimation2() {
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
             currentIndex = (currentIndex + 1) % colours.count
         }
     }
