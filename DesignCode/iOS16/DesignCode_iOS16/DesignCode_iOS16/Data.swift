3c915bfb0e6d449875ad87bc80effa3acfed72db
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-05 06:50:20 +0530

    DesignCode_iOS16: 1.added Data model and sample data

---
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/Data.swift
@@ -0,0 +1,22 @@
+//
+//  Data.swift
+//  DesignCode_iOS16
+//
+//  Created by Siddharth Kothari on 05/09/24.
+//
+
+import Foundation
+
+struct NavigationItem: Identifiable {
+    var id = UUID()
+    var title: String
+    var icon: String
+}
+
+var navigationItems = [
+    NavigationItem(title: "Trash", icon: "trash"),
+    NavigationItem(title: "Pencil", icon: "pencil"),
+    NavigationItem(title: "Eraser", icon: "eraser"),
+    NavigationItem(title: "Lasso", icon: "lasso"),
+    NavigationItem(title: "Folder", icon: "folder")
+]
