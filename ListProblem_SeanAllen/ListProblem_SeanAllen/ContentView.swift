acf15b7cbfcd7acbbd42ab54b808096e9efb6e0b
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-10 19:47:22 +0530

    ListProblem: initial flow

---
+++ ListProblem_SeanAllen/ListProblem_SeanAllen/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  ListProblem_SeanAllen
+//
+//  Created by Siddharth Kothari on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            List {
+                ForEach(0..<20) { _ in
+                    HStack(alignment: .top, spacing: 15, content: {
+                        RoundedRectangle(cornerRadius: 15, style: .continuous)
+                            .frame(width: 80, height: 80)
+                            .foregroundStyle(.cyan.gradient.opacity(0.5))
+                        
+                        Text("Test title")
+                    })
+                }
+            }
+            
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.red.opacity(0.3))
+                .frame(width: .infinity, height: 50)
+                .padding()
+        }
+        .navigationTitle("List Problem")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListProblem_SeanAllen/ListProblem_SeanAllen/ListProblem_SeanAllenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListProblem_SeanAllenApp.swift
+//  ListProblem_SeanAllen
+//
+//  Created by Siddharth Kothari on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ListProblem_SeanAllenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
