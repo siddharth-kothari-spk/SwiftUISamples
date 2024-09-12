38c713339d8c40875f897d8f6d5046bb62132e63
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-12 19:26:39 +0530

    DesignCode_iOS16: 1. applying gradient

--- DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ChartView.swift
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ChartView.swift
@@ -11,13 +11,12 @@ import Charts
 struct ChartView: View {
     var body: some View {
         Chart {
-            BarMark(x: .value("Day", "Sept 12"), y: .value("Value", 50))
+            BarMark(x: .value("Day", "Sept 11"), y: .value("Value", 50))
             BarMark(x: .value("Day", "Sept 12"), y: .value("Value", 40))
-                .foregroundStyle(.blue)
-            
             BarMark(x: .value("Day", "Sept 13"), y: .value("Value", 70))
-                .foregroundStyle(.green)
         }
+        .foregroundStyle(.linearGradient(colors: [.orange, .red, .yellow, .green, .cyan], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .frame(height: 300)
     }
 }
 
