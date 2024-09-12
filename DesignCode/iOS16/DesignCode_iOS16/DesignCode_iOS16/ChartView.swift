daab96b4b83459c007a3920419af1aa8cb84cad0
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-12 19:23:04 +0530

    DesignCode_iOS16: 1. different x-axis value , 2. same x-axis, different y-axis data

--- DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ChartView.swift
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/ChartView.swift
@@ -12,6 +12,11 @@ struct ChartView: View {
     var body: some View {
         Chart {
             BarMark(x: .value("Day", "Sept 12"), y: .value("Value", 50))
+            BarMark(x: .value("Day", "Sept 12"), y: .value("Value", 40))
+                .foregroundStyle(.blue)
+            
+            BarMark(x: .value("Day", "Sept 13"), y: .value("Value", 70))
+                .foregroundStyle(.green)
         }
     }
 }
