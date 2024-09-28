6ba81a0da778de497fa9dd31373ce8a45e4762d1
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-09-28 08:13:24 +0530

    Spatial Borders: initial courtsey commit

---
+++ Spatial_Borders/Spatial_Borders/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spatial_Borders
+//
+//  Created by Siddharth Kothari on 28/09/24.
+//
+
+// courtsey: https://uvolchyk.medium.com/exploring-spatial-borders-with-swiftui-0079fd1e0979
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spatial_Borders/Spatial_Borders/Spatial_BordersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spatial_BordersApp.swift
+//  Spatial_Borders
+//
+//  Created by Siddharth Kothari on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Spatial_BordersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
