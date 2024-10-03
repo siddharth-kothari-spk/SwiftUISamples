741ca866a92643c855fcdb7cfa9cac4bb82e994c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-10-03 06:52:45 +0530

    DesignCode_iOS16: 1. added radial custom layout using radius to palce subviews at angles

--- DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/RadialLayoutView.swift
+++ DesignCode/iOS16/DesignCode_iOS16/DesignCode_iOS16/RadialLayoutView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RadialLayoutView: View {
     var icons = ["calendar", "message", "figure", "calendar"]
     var body: some View {
-        CustomLayout {
+        RadialLayout {
             ForEach(icons, id:\.self) { item in
                 Circle()
                     .frame(width: 44)
@@ -42,6 +42,35 @@ struct CustomLayout: Layout {
             // position
             var point = CGPoint(x: 50 * index, y: 50 * index)
                 .applying(CGAffineTransform(rotationAngle: 30))
+            
+            // center
+            point.x += bounds.midX
+            point.y += bounds.midY
+            
+            subView.place(at: point,
+                          anchor: .center,
+                          proposal: .unspecified)
+        }
+    }
+}
+
+struct RadialLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+        /// Use the default value to prevent a flexible view from disappearing
+        /// into a zero-sized frame, and ensure the unspecified value remains
+        /// visible during debugging.
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        
+        let radius = bounds.width / 3.0
+        
+        for (index, subView) in subviews.enumerated() {
+            
+            // position
+            var point = CGPoint(x: 0, y: radius)
+                .applying(CGAffineTransform(rotationAngle: 60))
             
             // center
             point.x += bounds.midX
