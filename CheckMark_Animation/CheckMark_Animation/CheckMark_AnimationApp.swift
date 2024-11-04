1ac362d04f32c047eab2cc9551166ea2ae131e2d
siddharth-kothari-spk <siddharth.kothari.spk>
2024-11-04 07:24:13 +0530

    CheckMark_Animation: 1. added circles in Zstack, 2. added tap gesture with animation

---
+++ CheckMark_Animation/CheckMark_Animation/CheckMark_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMark_AnimationApp.swift
+//  CheckMark_Animation
+//
+//  Created by Siddharth Kothari on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckMark_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckMark_Animation/CheckMark_Animation/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  CheckMark_Animation
+//
+//  Created by Siddharth Kothari on 04/11/24.
+//
+// courtsey: sucodee
+// https://www.linkedin.com/posts/su-codee-676946321_swiftui-checkmark-animation-activity-7258812361073991680-vAAR/
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTapped = false
+    @State var isAnimated = false
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<6) { index in
+                Circle()
+                    .frame(width: 30, height: 30)
+            }
+        }
+        .foregroundStyle(isTapped ? .indigo : .blue)
+        .onTapGesture {
+            withAnimation(.spring(duration: 1)) {
+                isTapped.toggle()
+            }
+            withAnimation(isTapped ? .spring(duration: 1) : .none) {
+                isAnimated.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
