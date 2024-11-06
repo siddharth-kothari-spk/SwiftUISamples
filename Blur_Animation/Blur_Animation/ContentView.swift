42961cd2170184fbb20623e77c16d99438009b18
siddharth-kothari-spk <siddharth.kothari.spk>
2024-11-06 07:28:41 +0530

    Blur_Animation: added opacity and blur, sample video

--- Blur_Animation/Blur_Animation/ContentView.swift
+++ Blur_Animation/Blur_Animation/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
                 Text(String(alphabet))
                     .font(.largeTitle.bold())
                     .offset(y: isVisible ? 0 : -30)
+                    .opacity(isVisible ? 1 : 0)
+                    .blur(radius: isVisible ? 0 : 10)
                     .animation(
                         .easeOut(duration: 0.3)
                         .delay(Double(index) * 0.1), value: isVisible)
