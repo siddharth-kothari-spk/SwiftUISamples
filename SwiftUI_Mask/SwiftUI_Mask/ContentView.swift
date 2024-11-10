428b14a460aa009fa9ef9036048fd7a12e767745
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-10 18:27:13 +0530

    SwiftUI Mask: added circle in mask

--- SwiftUI_Mask/SwiftUI_Mask/ContentView.swift
+++ SwiftUI_Mask/SwiftUI_Mask/ContentView.swift
@@ -23,6 +23,10 @@ struct ContentView: View {
                 .stroke(lineWidth: 25)
                 .frame(width: 150, height: 150)
                 .foregroundStyle(AngularGradient.init(gradient: Gradient(colors: gradientColors), center: .center, angle: .degrees(0)))
+                .mask {
+                    Circle()
+                        .stroke(style: StrokeStyle(lineWidth: 25, lineCap: .round, lineJoin: .round))
+                }
         })
     }
 }
