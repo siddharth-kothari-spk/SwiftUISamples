75347e3ab522739de0a3eef5f7d70b1ed9c79478
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-15 08:13:58 +0530

    StackView_Animation: added dummy data

--- StackView_Animation/StackView_Animation/ContentView.swift
+++ StackView_Animation/StackView_Animation/ContentView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks: [DataModel] = [
+        DataModel(title: "Task A", isComplete: false),
+        DataModel(title: "Task B", isComplete: false),
+        DataModel(title: "Task C", isComplete: false),
+        DataModel(title: "Task D", isComplete: false),
+        DataModel(title: "Task E", isComplete: false),
+        DataModel(title: "Task F", isComplete: false),
+    ]
+    @State var show = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
