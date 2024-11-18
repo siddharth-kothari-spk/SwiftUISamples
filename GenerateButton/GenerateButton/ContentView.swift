f698dd3e9869346017cc6f6712513a1e5dcc308b
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-18 20:06:46 +0530

    Generate Button: added label and extra zstack

--- GenerateButton/GenerateButton/ContentView.swift
+++ GenerateButton/GenerateButton/ContentView.swift
@@ -18,17 +18,29 @@ struct ContentView: View {
             
             ZStack {
                 VStack {
-                    Capsule()
-                        .frame(width: 160, height: 55)
-                    .foregroundStyle(RadialGradient(colors: show ? [.orange, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 20, endRadius: 10))
-                    .shadow(color: show ? .orange : .black, radius: show ? 30 : 0, x: 1, y: 3)
-                    .padding()
+                    ZStack {
+                        Capsule()
+                            .frame(width: 160, height: 55)
+                        .foregroundStyle(RadialGradient(colors: show ? [.orange, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 20, endRadius: 10))
+                        .shadow(color: show ? .orange : .black, radius: show ? 30 : 0, x: 1, y: 3)
+                        .padding()
+                        
+                        Label("Generate", systemImage: "wand.and.stars.inverse")
+                            .bold()
+                            .foregroundStyle(show ? .white : .gray)
+                    }
                     
-                    Capsule()
-                        .frame(width: 160, height: 55)
-                    .foregroundStyle(RadialGradient(colors: show ? [.red, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 10, endRadius: -10))
-                    .shadow(color: show ? .red : .black, radius: show ? 10 : 0, x: 3, y: 1)
-                    .padding()
+                    ZStack {
+                        Capsule()
+                            .frame(width: 160, height: 55)
+                        .foregroundStyle(RadialGradient(colors: show ? [.red, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 10, endRadius: -10))
+                        .shadow(color: show ? .red : .black, radius: show ? 10 : 0, x: 3, y: 1)
+                        .padding()
+                        
+                        Label("Generate", systemImage: "wand.and.stars.inverse")
+                            .bold()
+                            .foregroundStyle(show ? .black : .white)
+                    }
                 }
             }
         }
