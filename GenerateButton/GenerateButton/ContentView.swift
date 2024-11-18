7e3950ce5864b0e7307fc9ccd108ca2d0475f118
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-18 20:02:05 +0530

    Generate Button: shadow added to capsule

--- GenerateButton/GenerateButton/ContentView.swift
+++ GenerateButton/GenerateButton/ContentView.swift
@@ -21,10 +21,14 @@ struct ContentView: View {
                     Capsule()
                         .frame(width: 160, height: 55)
                     .foregroundStyle(RadialGradient(colors: show ? [.orange, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 20, endRadius: 10))
+                    .shadow(color: show ? .orange : .black, radius: show ? 30 : 0, x: 1, y: 3)
+                    .padding()
                     
                     Capsule()
                         .frame(width: 160, height: 55)
-                    .foregroundStyle(RadialGradient(colors: show ? [.orange, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 10, endRadius: -10))
+                    .foregroundStyle(RadialGradient(colors: show ? [.red, .white] : [.gray.opacity(0.2)], center: .bottom, startRadius: 10, endRadius: -10))
+                    .shadow(color: show ? .red : .black, radius: show ? 10 : 0, x: 3, y: 1)
+                    .padding()
                 }
             }
         }
