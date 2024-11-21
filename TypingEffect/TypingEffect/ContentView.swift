903356143445a37ae91bbf006499046101927f6e
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-21 06:15:17 +0530

    TypingEffect: added state var and UI

---
+++ TypingEffect/TypingEffect/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  TypingEffect
+//
+//  Created by Siddharth Kothari on 21/11/24.
+//
+
+// https://www.linkedin.com/posts/su-codee-676946321_swiftui-iosdeveloper-swift-activity-7264978645629173761-U2R2/?utm_source=share&utm_medium=member_ios
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var displayItem = ""
+    @State var show = false
+
+    let fullItem = "My name is Nikku"
+    let typingSpeed = 0.15
+    
+    var body: some View {
+        HStack {
+            Text(displayItem)
+                .font(.largeTitle.monospacedDigit())
+            Rectangle()
+                .frame(width: 5, height: 50)
+        }
+        .foregroundStyle(.orange.opacity(0.8))
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TypingEffect/TypingEffect/TypingEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypingEffectApp.swift
+//  TypingEffect
+//
+//  Created by Siddharth Kothari on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TypingEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
