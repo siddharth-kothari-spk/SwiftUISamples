e35964dda88d952bb5de210116971ab513031209
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-23 11:31:48 +0530

    CustomTextFieldAnimation: InfoTextField setup

---
+++ CustomTextFieldAnimation/CustomTextFieldAnimation/InfoTextField.swift
@@ -0,0 +1,29 @@
+//
+//  InfoTextField.swift
+//  CustomTextFieldAnimation
+//
+//  Created by Siddharth Kothari on 23/11/24.
+//
+
+import SwiftUI
+
+struct InfoTextField: View {
+    var title: String
+    @Binding var text: String
+    /// A property wrapper type that can read and write a value owned by a source of truth.
+    @FocusState var isActive
+    var body: some View {
+        ZStack(alignment: .leading) {
+            TextField("", text: $text)
+                .padding(.leading)
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .focused($isActive)
+                .background(.gray.opacity(0.3), in: .rect(cornerRadius: 16))
+        }
+    }
+}
+
+//#Preview {
+//    InfoTextField()
+//}
