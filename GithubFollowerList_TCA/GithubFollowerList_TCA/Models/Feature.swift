7fa9d7d42dafa7a14e34d15d923916440061e9fa
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-11-30 07:58:31 +0530

    GithubFollowerList_TCA: added basic Reducer with its components

--- GithubFollowerList_TCA/GithubFollowerList_TCA/Models/Feature.swift
+++ GithubFollowerList_TCA/GithubFollowerList_TCA/Models/Feature.swift
@@ -15,3 +15,43 @@ struct Follower: Identifiable {
     let htmlUrl: URL
 }
 
+@Reducer
+struct FollowerFeature {
+    // if feature is to be observed by SwiftUI, which is usually the case, you must annotate its state with the ObservableState() macro. It is the Composable Architecture’s version of @Observable, but tuned to value types
+    @ObservableState
+    // create a State type that holds the state your feature needs to do its job. generally struct
+    struct State {
+        var isLoading = false
+        var followers: [Follower] = []
+        var error: Error?
+    }
+    // create an Action type that holds all the actions the user can perform in the feature. generally enum
+    enum Action {
+        case fetchData
+        case fetchDataSuccess([Follower])
+        case fetchDataFailure(Error)
+        case selectFollower(Follower)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            case .fetchData:
+                print("add code to fetch data")
+                return .none // currently returning none, will be updated as we progress
+            case .fetchDataSuccess(let followers):
+                print("fetch data success")
+                return .none
+            case .fetchDataFailure(let error):
+                print("fetch data failure")
+                return .none
+            case .selectFollower(let follower):
+                print("select follower")
+                return .none
+            }
+            
+        }
+    }
+    
+}
