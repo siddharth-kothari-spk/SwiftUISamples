98bb12b508b135d393b535fcbbbf64e6759ef9ba
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-01 08:32:55 +0530

    GithubFollowerList_TCA: added DataError enum

---
+++ GithubFollowerList_TCA/GithubFollowerList_TCA/Models/DataError.swift
@@ -0,0 +1,12 @@
+//
+//  DataError.swift
+//  GithubFollowerList_TCA
+//
+//  Created by Siddharth Kothari on 01/12/24.
+//
+
+import Foundation
+
+enum DataError: Error {
+    case fetchDataFailure
+}
