0aaf67bad26944b203bb725caa1cbe3fcdcff8ba
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-08 12:41:08 +0530

    HeartAnimation: initial commit

---
+++ HeartAnimation/HeartAnimation/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  HeartAnimation
+//
+//  Created by Siddharth Kothari on 08/12/24.
+//
+
+// https://www.linkedin.com/posts/su-codee-676946321_swiftui-heart-animation-activity-7270383909991780352-7ysf/
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartAnimation/HeartAnimation/HeartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartAnimationApp.swift
+//  HeartAnimation
+//
+//  Created by Siddharth Kothari on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
