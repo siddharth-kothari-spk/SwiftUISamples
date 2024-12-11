e721a74fdec19b79e41aa58efd25d9b09f618ab9
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-11 18:09:06 +0530

    ShimmerEffect2: initial commit

---
+++ ShimmerEffect2/ShimmerEffect2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShimmerEffect2
+//
+//  Created by Siddharth Kothari on 11/12/24.
+//
+
+// https://www.linkedin.com/posts/su-codee-676946321_swiftui-shimmereffect-with-textrenderer-activity-7272558294156603393-9-wy?utm_source=share&utm_medium=member_desktop
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShimmerEffect2/ShimmerEffect2/ShimmerEffect2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffect2App.swift
+//  ShimmerEffect2
+//
+//  Created by Siddharth Kothari on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffect2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
