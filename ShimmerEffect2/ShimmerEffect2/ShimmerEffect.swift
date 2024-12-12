cd1ffd94e088e5642c926d61d020b6359e003b9b
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-12 07:39:58 +0530

    ShimmerEffect2: comments added

--- ShimmerEffect2/ShimmerEffect2/ShimmerEffect.swift
+++ ShimmerEffect2/ShimmerEffect2/ShimmerEffect.swift
@@ -18,11 +18,15 @@ struct ShimmerEffect: TextRenderer {
     }
     // avaialble from iOS 17 onwards , 2 more methods came from iOS 18
     func draw(layout: Text.Layout, in ctx: inout GraphicsContext) {
+        /// layout: This parameter provides information about the layout of the text, such as the positions and sizes of individual glyphs.
+        /// ctx: This parameter represents the graphics context where the text will be drawn.
+        
+        /// Method creates a shimmering effect. It does this by dynamically adjusting the opacity of each glyph within the text based on its position and the current animation progress. This results in a visual effect where the text appears to be shimmering or highlighting as the animation progresses.
         for line in layout {
             for runs in line {
                 for (index, glyph) in runs.enumerated() {
-                    let relativePosition = CGFloat(index) / CGFloat(runs.count)
-                    let adjustedOpacity = max(0, 1 - abs(relativePosition - animationInProgress))
+                    let relativePosition = CGFloat(index) / CGFloat(runs.count) // This variable calculates the relative position of the current glyph within its run.
+                    let adjustedOpacity = max(0, 1 - abs(relativePosition - animationInProgress)) // This ensures that the opacity smoothly transitions from 0 to 1 and back to 0 as the animation progresses.
                     ctx.opacity = adjustedOpacity
                     ctx.draw(glyph)
                 }
