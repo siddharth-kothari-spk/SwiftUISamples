8db1e09e9927a001c7e3e98e267f5a418f744f4f
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-21 16:31:07 +0530

    TimeLineViewSample: using TimelineViewDefaultContext to create a circle animating time lapsing

--- TimeLineViewSample/TimeLineViewSample/ContentView.swift
+++ TimeLineViewSample/TimeLineViewSample/ContentView.swift
@@ -4,19 +4,24 @@
 //
 //  Created by Siddharth Kothari on 21/12/24.
 //
+// https://swiftwithmajid.com/2022/05/18/mastering-timelineview-in-swiftui/
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TimelineView(.animation) { timeLineViewContext in
+            let value = secondsValue(for: timeLineViewContext.date)
+            Circle()
+                .trim(from: 0, to: value)
+                .stroke(lineWidth: 5)
+                .padding()
         }
-        .padding()
     }
+    private func secondsValue(for date: Date) -> Double {
+            let seconds = Calendar.current.component(.second, from: date)
+            return Double(seconds) / 60
+        }
 }
 
 #Preview {
