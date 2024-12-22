f2776b7e0fe15d6072818640d4a1d292955feb7a
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-22 07:42:10 +0530

    TimeLineViewSample: using cadence to the rate at which TimelineView updates

--- TimeLineViewSample/TimeLineViewSample/ContentView.swift
+++ TimeLineViewSample/TimeLineViewSample/ContentView.swift
@@ -18,12 +18,31 @@ struct ContentView: View {
                     .trim(from: 0, to: value)
                     .stroke(lineWidth: 5)
                     .padding()
+                    .frame(width: 200, height: 200)
             }
+            
+            // The cadence represents the rate at which TimelineView updates, and it might change many times during the view’s lifecycle. For example, running the TimelineView on Apple Watch might decrease cadence while the user lowers the wrist. Fortunately, the Cadence type conforms to Comparable protocol, and we can easily compare them.
+            TimelineView(.animation) { context in
+                       let date = context.date
+                       let value = context.cadence >= .live ?
+                           nanosValue(for: date): secondsValue(for: date)
+           // .live: Updates the view continuously.
+                       Circle()
+                    .trim(from: 0, to: value)
+                           .stroke()
+                           .frame(width: 200, height: 200)
+                   }
         }
     }
     private func secondsValue(for date: Date) -> Double {
             let seconds = Calendar.current.component(.second, from: date)
             return Double(seconds) / 60
+        }
+    
+    private func nanosValue(for date: Date) -> Double {
+            let seconds = Calendar.current.component(.second, from: date)
+            let nanos = Calendar.current.component(.nanosecond, from: date)
+            return Double(seconds * 1_000_000_000 + nanos) / 60_000_000_000
         }
 }
 
