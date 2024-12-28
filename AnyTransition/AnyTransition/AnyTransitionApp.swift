4711a1b21ed35bf9ac7a5fcf734145837a008b9a
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-28 18:39:03 +0530

    AnyTransition: initial commit

---
+++ AnyTransition/AnyTransition/AnyTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyTransitionApp.swift
+//  AnyTransition
+//
+//  Created by Siddharth Kothari on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AnyTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnyTransition/AnyTransitionTests/AnyTransitionTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnyTransitionTests.swift
+//  AnyTransitionTests
+//
+//  Created by Siddharth Kothari on 28/12/24.
+//
+
+import Testing
+@testable import AnyTransition
+
+struct AnyTransitionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
