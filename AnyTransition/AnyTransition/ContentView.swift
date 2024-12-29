a0a81e95bab35de04de8dfc27666fd8c2f376171
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-29 07:53:58 +0530

    AnyTransition: adding new block to show custon tramsition combined with new transition

--- AnyTransition/AnyTransition/ContentView.swift
+++ AnyTransition/AnyTransition/ContentView.swift
@@ -49,6 +49,30 @@ struct ContentView: View {
             .onAppear {
                 startTimer()
             }
+            
+            HStack(spacing: 4) {
+                Text("Loading")
+                
+                ZStack {
+                    ForEach(0..<items.count, id: \.self) { index in
+                        if index == currentIndex {
+                            Text(items[index])
+                                .bold()
+                                .foregroundStyle(colors[index])
+                                .transition(customTransition.combined(with: .scale(scale: 2.0, anchor: .leading)))
+                            // .combined: Combines this transition with another, returning a new transition that is the result of both transitions being applied.
+                            // .scale: Returns a transition that scales the view by the specified amount.
+                            
+                        }
+                    }
+                }
+                .frame(width: 100, height: 50, alignment: .leading)
+                .clipped() // used to see items in height 50 only
+                .padding(EdgeInsets(top: 10, leading: 5, bottom: 5, trailing: 5))
+            }
+            .onAppear {
+                startTimer()
+            }
         }
     }
 }
